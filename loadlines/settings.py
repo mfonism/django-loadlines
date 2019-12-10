7c64cd7d6d0e9794f180019adfaecf5affbc06ee
Mfon Eti-mfon <mfonetimfon@gmail.com>
2019-12-10 11:21:48 +0100

    Initial project setup

---
+++ loadlines/settings.py
@@ -0,0 +1,14 @@
+import pathlib
+
+BASE_DIR = pathlib.Path(__file__).absolute().parent.parent
+
+SECRET_KEY = "5jf7o$aomj_*+2wl-+po7*d(zs4nf_jf2r8sgvf_4x2rp3v#0_"
+
+INSTALLED_APPS = ["core.apps.CoreConfig"]
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": str(BASE_DIR.joinpath("db.sqlite3")),
+    }
+}
